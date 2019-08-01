c7d2b9cf9ba9aa04538c30188a5d54424371887f
xpro <langxm682@gmail.com>
2019-08-01 19:44:05 +0800

    添加文件
    或许不应该添加pyqt5，

---
+++ main.py
@@ -0,0 +1,25 @@
+import sys
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtCore import pyqtSignal, QObject
+
+class Communicate(QObject):
+    closeApp = pyqtSignal()
+
+class Example(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.init()
+
+    def init(self):
+        self.c = Communicate()
+        self.c.closeApp.connect(self.close)
+        self.setGeometry(300, 300, 300, 300)
+        self.setWindowTitle('emit signal')
+        self.show()
+
+    def mousePressEvent(self, event):
+        self.c.closeApp.emit()
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
